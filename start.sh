20d7673a69f658758bb0c888ba7e6df9dce4a3b0
camjac251 <camjac251@users.noreply.github.com>
2020-08-28 16:46:53 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,12 +14,15 @@ chown -R nobody:users /home
 
 # install ffmpeg
 wget https://raw.githubusercontent.com/q3aql/ffmpeg-install/master/ffmpeg-install -O /config/ffmpeg-install
-chmod a+x ffmpeg-install
+chmod a+x /config/ffmpeg-install
 /config/ffmpeg-install --install
 /config/ffmpeg-install --update
 
 cp /ffmpeg-container-change.sh /config/ffmpeg-container-change.sh
 chown -R nobody:users /config
+
+echo "[Info] Entering work folder"
+cd /work
 
 echo "[Info] Starting script"
 bash /config/ffmpeg-container-change.sh
