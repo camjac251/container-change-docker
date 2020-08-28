dbd42313314eae80b685e27f672981de8f7fbe5e
camjac251 <camjac251@users.noreply.github.com>
2020-08-28 04:20:10 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,21 +3,20 @@
 #Give message when starting the container
 printf "\n \n \n ------------------------Starting container ------------------------ \n \n \n"
 
-#If convert2mkv.sh exist, then skip redownloading it.
-if [[ -f "/config/convert2mkv.sh" ]]; then
- 
- echo "[Info] Script already downloaded, skipping download. If you want to redownload the script, delete convert2mkv.sh in you appdata"
+# Configure user nobody to match unRAID's settings
+#export DEBIAN_FRONTEND="noninteractive"
+usermod -u 99 nobody
+usermod -g 100 nobody
+usermod -d /home nobody
+chown -R nobody:users /home
 
-#Downloading script if it is not found in /config
-else
- echo "[Info] Downloading script..."
- curl https://gitlab.com/ThatGuy/convert2mkv/raw/master/convert2mkv.sh > /config/convert2mkv.sh && \
- chmod +x /config/convert2mkv.sh
- chmod 777 /config/convert2mkv.sh
- 
- fi
- 
- echo "[Info] Starting script"
- bash /config/convert2mkv.sh
- 
+#chsh -s /bin/bash nobody
+
+cp /convert2mkv.sh /config/convert2mkv.sh
+chown -R nobody:users /config
+
+echo "[Info] Starting script"
+bash /config/convert2mkv.sh
+#su - nobody -c /config/convert2mkv.sh
+
 echo "Stopping Container, script finished.."
